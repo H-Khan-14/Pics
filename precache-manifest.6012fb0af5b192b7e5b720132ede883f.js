5062598faf89d8c0243607fe94d5f0edc59dbd6f
H-Khan-14 <haseebkhan14@hotmail.com>
2021-09-09 16:50:49 -0400

    Updates

---
+++ precache-manifest.6012fb0af5b192b7e5b720132ede883f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63a843e992325cfeacd6",
+    "url": "/Pics/static/js/runtime~main.63a843e9.js"
+  },
+  {
+    "revision": "5eeb59506208090cb51e",
+    "url": "/Pics/static/js/main.5eeb5950.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/Pics/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "5eeb59506208090cb51e",
+    "url": "/Pics/static/css/main.19aa5ca0.chunk.css"
+  },
+  {
+    "revision": "d0d8c7aec3db975c05dad9ca203b2974",
+    "url": "/Pics/index.html"
+  }
+];
